8294698ae0d4431790aa8206def31288bd7bb879
ByeonggeunSon <qudrms007@naver.com>
2020-02-04 01:03:51 +0900

    CreateOrderInteractor 생성

---
+++ CleanStore/CreateOrderInteractor.swift
@@ -0,0 +1,34 @@
+//
+//  CreateOrderInteractor.swift
+//  CleanStore
+//
+//  Created by byeonggeunSon on 2020/02/04.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import UIKit
+
+protocol CreateOrderInteractorInput{
+//    func doSomething(request: CreateOrderRequest)
+}
+
+protocol CreateOrderInteractorOutput{
+//    func presentSomething(response: CreateOrderResponse)
+}
+
+class CreateOrderInteractor: CreateOrderInteractorInput{
+    var output: CreateOrderInteractorOutput!
+//    var worker: CreateOrderWorker
+    
+    //MARK: Business Logic
+    
+//    func doSomething(request: CreateOrderRequest){
+//        worker = CreateOrderWorker()
+//        worker.doSomeWork()
+//
+//        let response = CreateOrderResponse()
+//        output.presentSomething(response)
+//    }
+    
+    
+}
