0f9df66b96e1549d704165455ebaf53e86bf1f1e
ByeonggeunSon <qudrms007@naver.com>
2020-02-05 20:37:56 +0900

    ShowOrderViewController에서 Edit Button 클릭시 CreateViewController의 interactor의 orderToEdit 지정 및 Scene 호출 라우팅 로직 생성

--- CleanStore/Scenes/ShowOrder/ShowOrderRouter.swift
+++ CleanStore/Scenes/ShowOrder/ShowOrderRouter.swift
@@ -6,10 +6,10 @@
 //  Copyright © 2020 손병근. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 @objc protocol ShowOrderRoutingLogic{
-    
+    func routeToEditOrder(segue: UIStoryboardSegue?)
 }
 
 protocol ShowOrderDataPassing{
@@ -19,4 +19,28 @@ protocol ShowOrderDataPassing{
 class ShowOrderRouter: NSObject, ShowOrderRoutingLogic, ShowOrderDataPassing{
     weak var viewController: ShowOrderViewController?
     var dataStore: ShowOrderDataStore?
+    
+    //MARK: - Routing
+    func routeToEditOrder(segue: UIStoryboardSegue?) {
+        if let segue = segue{
+            let destinationVC = segue.destination as! CreateOrderViewController
+            var destinationDS = destinationVC.router!.dataStore!
+            passDataToCreateOrder(source: dataStore!, destination: &destinationDS)
+        }else{
+            let destinationVC = viewController?.storyboard?.instantiateViewController(withIdentifier: "CreateOrderViewController") as! CreateOrderViewController
+            var destinationDS = destinationVC.router!.dataStore!
+            passDataToCreateOrder(source: dataStore!, destination: &destinationDS)
+            navigateToCreateOrder(source: viewController!, destination: destinationVC)
+        }
+    }
+    
+    //MARK: Navigation Logic
+    func navigateToCreateOrder(source: ShowOrderViewController, destination: CreateOrderViewController){
+        source.show(destination, sender: nil)
+    }
+    
+    //MARK: Passing Data
+    func passDataToCreateOrder(source: ShowOrderDataStore, destination: inout CreateOrderDataStore){
+        destination.orderToEdit = source.order
+    }
 }
